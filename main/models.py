e14fe74e2533d6d8e2fefd4f90ed488623a88d7d
Johnny Osei Aidoo <johnnyoseiaidoo65@gmail.com>
2023-01-18 14:38:30 +0000

    'deploy'

--- main/models.py
+++ main/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-import jwt
-
 class Posts(models.Model):
     postImage = models.ImageField(max_length=10000, default='noImg.png')
     postTitle = models.CharField(max_length=200, default='free')
