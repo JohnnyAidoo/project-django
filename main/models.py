4f874e534db7d4e880c3ffba77fba86d991b15a7
Johnny Osei Aidoo <johnnyoseiaidoo65@gmail.com>
2023-01-20 09:40:07 +0000

    'descriptions added'

--- main/models.py
+++ main/models.py
@@ -6,5 +6,6 @@ class Posts(models.Model):
     postImage = models.ImageField(max_length=10000, default='noImg.png')
     postTitle = models.CharField(max_length=200, default='free')
     postPrice = models.IntegerField()
+    postDescription = models.CharField(max_length=1000000,default='NO DESCRIPTION ADDED', blank=False)
     postCategory = models.CharField(max_length=200,default='others')
 
